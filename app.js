fe6ce724b8aaa629be50ece8e6b9364e56cf893c
Alexei Pancratov <drivergear@gmail.com>
2021-07-05 22:38:51 -0400

    Implemented money receive

--- app.js
+++ app.js
@@ -8,15 +8,25 @@ const rl = readline.createInterface({
 
 rl.question("Enter RippleX account ID: ", async (rippleAccountId) => {
   const balanceData = await interledgerService.getBalanceData(rippleAccountId);
+  
   console.log(`Balance of ${rippleAccountId} is ${balanceData.accountBalance.netBalance}`);
 
   rl.question("Enter a rafiki account to send money to: ", async (rafikiAccountId) => {
     const transferData = await interledgerService.payFriend(rippleAccountId, rafikiAccountId);
+
     console.log("=====Receipt Start=====");
     console.log(`Original amount: ${transferData.originalAmount}`);
     console.log(`Amount delivered: ${transferData.amountDelivered}`);
     console.log(`Amount sent: ${transferData.amountSent}`);
     console.log(`Payment is ${transferData.successfulPayment ? "successful" : "unsuccessful"}`);
     console.log("=====Receipt End=====");
+
+    rl.question("Press any key to receive money back", async () => {
+      const transferData = await interledgerService.receiveMoney(rippleAccountId);
+
+      console.log("=====Receipt Start=====");
+      console.log(`Sent ${transferData.sent}`);
+      console.log("=====Receipt End=====");
+    });
   });
 });
--- interledger-service.js
+++ interledger-service.js
@@ -1,10 +1,12 @@
 const axios = require("axios").default;
 const config = require("./config.json");
 
-axios.defaults.headers.common["Authorization"] = `Bearer ${config.apiBearerToken}`;
-
 async function getBalanceData(accountId) {
-  const response = await axios.get(`https://ripplex.io/portal/ilp/hermes/accounts/${accountId}/balance`);
+  const response = await axios.get(`https://ripplex.io/portal/ilp/hermes/accounts/${accountId}/balance`, {
+    headers: {
+      'Authorization': `Bearer ${config.rippleBearerToken}`
+    }
+  });
 
   return response.data;
 }
@@ -13,6 +15,25 @@ async function payFriend(from, to) {
   const response = await axios.post(`https://ripplex.io/portal/ilp/hermes/accounts/${from}/pay`, {
     amount: 1000,
     destinationPaymentPointer: `$rafiki.money/p/${to}`
+  }, {
+    headers: {
+      'Authorization': `Bearer ${config.rippleBearerToken}`
+    }
+  });
+
+  return response.data;
+}
+
+async function receiveMoney(rippleReceiver) {
+  const response = await axios.post("https://rafiki.money/api//payments/peer", {
+    accountId: 1211,
+    amount: "1000000",
+    receiverPaymentPointer: `$ripplex.money/${rippleReceiver}`,
+    type: "spsp"
+  }, {
+    headers: {
+      'Authorization': `Bearer ${config.rafikiBearerToken}`
+    }
   });
 
   return response.data;
@@ -20,5 +41,6 @@ async function payFriend(from, to) {
 
 module.exports = {
   getBalanceData,
-  payFriend
+  payFriend,
+  receiveMoney
 };
